5234fb5bcc4abd580feb4144130a3beef6c03311
mateusz.fijalkowski <mateusz.fijalkowski@makolab.com>
2021-06-26 15:38:12 +0200

    Add Handle Crud for Activity

---
+++ Application/Activities/List.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class List
+    {
+        public class Query : IRequest<List<Activity>> { }
+        public class Handler : IRequestHandler<Query, List<Activity>>
+        {
+            private readonly DataContext _contex;
+            public Handler(DataContext contex)
+            {
+                _contex = contex;
+            }
+
+            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return await _contex.Activities.ToListAsync(cancellationToken);
+            }
+        }
+    }
+}
